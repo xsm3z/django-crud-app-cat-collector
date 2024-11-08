bc91de788efe817098633b8913fbd3e7adf373ef
xsm3z <xsm.zhang@gmail.com>
2024-11-07 20:13:15 -0500

    connected sql

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,4 +4,5 @@ from . import views # Import views to connect routes to view functions
 urlpatterns = [
   path('', views.home, name='home'),
   path('about/', views.about, name='about'),
+  path('cats/', views.cat_index, name='cat-index'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,12 +1,29 @@
 from django.shortcuts import render
 
 # Import HttpResponse to send text-based responses
-from django.http import HttpResponse
+#from django.http import HttpResponse
 
 # Create your views here.
 
 def home(request):
-  return render(request, 'index.html')
+  return render(request, 'home.html')
 
 def about(request):
   return render(request, 'about.html')
+
+class Cat:
+  def __init__(self, name, breed, description, age):
+    self.name = name
+    self.breed = breed
+    self.description = description 
+    self.age = age
+
+cats = [
+    Cat('Lolo', 'tabby', 'Kinda rude.', 3),
+    Cat('Sachi', 'tortoiseshell', 'Looks like a turtle.', 0),
+    Cat('Fancy', 'bombay', 'Happy fluff ball.', 4),
+    Cat('Bonk', 'selkirk rex', 'Meows loudly.', 6)
+]
+
+def cat_index(request):
+  return render(request, 'cats/index.html', {'cats':cats})
