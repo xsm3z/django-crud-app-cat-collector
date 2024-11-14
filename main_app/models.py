70b7b78b757e9312fe05361f07e1a61b39cad546
xsm3z <xsm.zhang@gmail.com>
2024-11-14 18:34:25 -0500

    full CRUD

--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,11 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+MEALS = (
+    ('B', 'Breakfast'),
+    ('L', 'Lunch'),
+    ('D', 'Dinner')
+)
 
 class Cat(models.Model):
     name = models.CharField(max_length=100)
@@ -10,3 +15,16 @@ class Cat(models.Model):
 
     def __str__(self):
         return self.name
+
+    # Define a method to get the URL for this particular cat instance
+    def get_absolute_url(self):
+        # Use the 'reverse' function to dynamically find the URL for viewing this cat's details
+        return reverse('cat-detail', kwargs={'cat_id': self.id})
+
+class Feeding(models.Model):
+    date = models.DateField()
+    meal = models.CharField(
+        max_length=1,
+        choices=MEALS,
+        default=MEALS[0][0]
+        )
--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+from .models import Cat
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
 # Import HttpResponse to send text-based responses
-#from django.http import HttpResponse
+from django.http import HttpResponse
 
 # Create your views here.
 
@@ -11,19 +13,23 @@ def home(request):
 def about(request):
   return render(request, 'about.html')
 
-class Cat:
-  def __init__(self, name, breed, description, age):
-    self.name = name
-    self.breed = breed
-    self.description = description 
-    self.age = age
-
-cats = [
-    Cat('Lolo', 'tabby', 'Kinda rude.', 3),
-    Cat('Sachi', 'tortoiseshell', 'Looks like a turtle.', 0),
-    Cat('Fancy', 'bombay', 'Happy fluff ball.', 4),
-    Cat('Bonk', 'selkirk rex', 'Meows loudly.', 6)
-]
-
 def cat_index(request):
+  cats = Cat.objects.all()
   return render(request, 'cats/index.html', {'cats':cats})
+
+def cat_detail(request, cat_id):
+  cat = Cat.objects.get(id=cat_id)
+  return render(request, 'cats/detail.html', {'cat': cat})
+
+class CatCreate(CreateView):
+  model = Cat
+  fields = "__all__"
+  success_url = '/cats/'
+
+class CatUpdate(UpdateView):
+  model = Cat
+  fields = ['breed', 'description', 'age']
+
+class CatDelete(DeleteView):
+  model = Cat
+  success_url = '/cats/'
