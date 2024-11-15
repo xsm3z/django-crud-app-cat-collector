74fbb1593428d708cd013245bade55dc3bbf6e6f
xsm3z <xsm.zhang@gmail.com>
2024-11-14 21:40:11 -0500

    Feeding model added

--- main_app/models.py
+++ main_app/models.py
@@ -16,15 +16,21 @@ class Cat(models.Model):
     def __str__(self):
         return self.name
 
-    # Define a method to get the URL for this particular cat instance
     def get_absolute_url(self):
-        # Use the 'reverse' function to dynamically find the URL for viewing this cat's details
         return reverse('cat-detail', kwargs={'cat_id': self.id})
 
 class Feeding(models.Model):
-    date = models.DateField()
+    date = models.DateField('Feeding Date')
     meal = models.CharField(
         max_length=1,
         choices=MEALS,
         default=MEALS[0][0]
         )
+    
+    cat = models.ForeignKey(Cat, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return f"{self.get_meal_display()} on {self.date}"
+
+    class Meta:
+        ordering = ['-date']
--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Cat
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from .forms import FeedingForm
+
 
 # Import HttpResponse to send text-based responses
 from django.http import HttpResponse
@@ -18,8 +20,19 @@ def cat_index(request):
   return render(request, 'cats/index.html', {'cats':cats})
 
 def cat_detail(request, cat_id):
-  cat = Cat.objects.get(id=cat_id)
-  return render(request, 'cats/detail.html', {'cat': cat})
+    cat = Cat.objects.get(id=cat_id)
+    feeding_form = FeedingForm()
+    return render(request, 'cats/detail.html', {
+        'cat': cat, 'feeding_form': feeding_form
+    })
+
+def add_feeding(request, cat_id):
+    form = FeedingForm(request.POST)
+    if form.is_valid():
+        new_feeding = form.save(commit=False)
+        new_feeding.cat_id = cat_id
+        new_feeding.save()
+    return redirect('cat-detail', cat_id=cat_id)
 
 class CatCreate(CreateView):
   model = Cat
