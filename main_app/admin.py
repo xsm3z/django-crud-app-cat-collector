2c2ff899f19ed5d5e52c29757259c8d803f96dbd
xsm3z <xsm.zhang@gmail.com>
2024-11-18 18:50:10 -0500

    added third model, Toys

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 
-from .models import Cat, Feeding
+from .models import Cat, Feeding, Toy
 # Register your models here.
 
 admin.site.register(Cat)
 
 admin.site.register(Feeding)
+
+admin.site.register(Toy)
--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
-from .models import Cat
+from .models import Cat, Toy
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic import ListView, DetailView
 from .forms import FeedingForm
 
 
@@ -46,3 +47,21 @@ class CatUpdate(UpdateView):
 class CatDelete(DeleteView):
   model = Cat
   success_url = '/cats/'
+
+class ToyCreate(CreateView):
+    model = Toy
+    fields = '__all__'
+
+class ToyList(ListView):
+    model = Toy
+
+class ToyDetail(DetailView):
+    model = Toy
+
+class ToyUpdate(UpdateView):
+    model = Toy
+    fields = ['name', 'color']
+
+class ToyDelete(DeleteView):
+    model = Toy
+    success_url = '/toys/'
